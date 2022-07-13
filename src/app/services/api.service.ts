580f9913407e8f892714dc0bee92d45377db3eb3
Hapack <hapackcis@gmail.com>
2022-07-13 17:17:13 +0530

    first commit

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+  postDealer(data:any){
+    return this.http.post<any>("http://localhost:3000/posts",data).pipe(map((res:any)=>{
+      return res;
+    }))
+
+  }
+  
+
+
+  getDealer(){
+    return this.http.get<any>("http://localhost:3000/posts").pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+
+  updateDealer(data:any,id:number){
+    return this.http.put<any>("http://localhost:3000/posts/"+id,data).pipe(map((res:any)=>{
+      return res;
+    }))
+
+
+  }
+
+  
+  deleteDealer(id:number){
+    return this.http.delete<any>("http://localhost:3000/posts/"+id).pipe(map((res:any)=>{
+      return res;
+    }))
+
+
+  }
+
+
+}
